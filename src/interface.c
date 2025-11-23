a4ea6ff4484eff79d48258f244e8a7a3994f1ecf
livi_20 <liviacorrales05Qgmail.com>
2025-11-22 21:10:41 -0600

    V11: Delete game and create funtion for interface with buttons and state

--- src/interface.c
+++ src/interface.c
@@ -9,20 +9,40 @@ void dibujarCuadricula(WINDOW *win, Cuadricula *cuad) {
     getmaxyx(win, maxY, maxX);
 
     //dejamos el espacio para el titulo (fila arriba) y los botones (fila abajo)
-    int offsetY = 1;
-    int limiteY = maxY -2;
+    int offsetY = 2;
+    int alto_panel = 3;
+    int limiteY = maxY - alto_panel-1;
 
     //recorrer cada fila de la cuadrícula sin salirse de la matriz ni ponerse encima del titulo ni los botones
     for (unsigned short y = 0; y < cuad->alto && (y + offsetY) < limiteY; y++) {
         for (unsigned short x = 0; x < cuad->ancho && x < (unsigned short)maxX - 2; x++) {
             if (cuad->genActual[y][x])
-                mvwprintw(win, y + offsetY, x + 1, "█"); //célula viva
+                mvwprintw(win, y + offsetY, x + 1, "*"); //célula viva
             else
                 mvwprintw(win, y + offsetY, x + 1, " "); //célula muerta
         }
     }
 }
 
+//Se hace mejor una función para la caja del panel inferior
+void actualizarPanel(WINDOW *ventana, int inicio_panel, int win_ancho, int generacion, int delay_ms, int correr){
+    int fila_estado = inicio_panel +1;
+    int fila_botones = inicio_panel + 2;
+
+    //Para redibujar la línea superior
+    mvwhline(ventana, inicio_panel, 1, ACS_HLINE, win_ancho -2);
+
+    for (int x = 1; x < win_ancho -1; x++){
+        mvwaddch(ventana,fila_estado, x, ' ');
+        mvwaddch(ventana,fila_botones, x, ' ');
+    }
+
+    //Linea para los estados
+    mvwprintw(ventana, fila_estado, 2, "Gen:%d | Velocidad:%d ms | Estado: %s", generacion, delay_ms, correr ? "RUNNUNG":"PAUSADO");
+
+    //Linea para los botones
+    mvwprintw(ventana, fila_botones, 2, "[Q]:Salir | [P]:Play/Pausa | [+]:Acelerar | [-]:Desacelerar | [SPACE]:Avanzar gen");
+}
 
 int main (int argc, char ** argv) 
 {
@@ -51,20 +71,21 @@ int main (int argc, char ** argv)
     
     //Se hace un cambio para usar el tamaño de la ventana y no la terminal como lo tenía anteriormente.
     int win_altura, win_ancho;
+    getmaxyx(ventana, win_altura, win_ancho);
+
     int alto_panel = 3;
     int inicio_panel = win_altura - alto_panel -1; //Por el borde inferior
-    getmaxyx(ventana, win_altura, win_ancho);
     
-    mvwhline(ventana, 1, 1, 0, win_ancho -2); //poner lineas separadoras
-    mvwhline(ventana, inicio_panel, 1, 0, win_ancho -2);
-    
+    mvwhline(ventana, 1, 1, ACS_HLINE, win_ancho -2); //poner lineas separadoras
+   
+
     //Para botones dentro de la caja
     int botones = inicio_panel +1;
-    mvwprintw(ventana, botones, 2, "[Q]: Salir");
-    mvwprintw(ventana, botones, 12, "[P]: Play/Pausa");
-    mvwprintw(ventana, botones, 27, "[+]: Acelerar");
-    mvwprintw(ventana, botones, 40, "[-]: Desacelerar");
-    mvwprintw(ventana, botones, 56, "[SPACE]: Avanzar generación");
+    mvwprintw(ventana, botones, 2, "[Q]:Salir");
+    mvwprintw(ventana, botones, 12, "[P]:Play/Pausa");
+    mvwprintw(ventana, botones, 27, "[+]:Acelerar");
+    mvwprintw(ventana, botones, 40, "[-]:Desacelerar");
+    mvwprintw(ventana, botones, 57, "[SPACE]:Avanzar generación");
 
     dibujarCuadricula(ventana, cuadricula);
     wrefresh(ventana);
@@ -72,8 +93,12 @@ int main (int argc, char ** argv)
     //Variables para el estado del juego
     int correr = 0; // 0 = pausa, 1 = play
     int delay_ms = 200; //Tiempo entre generaciones en ms (Velocidad inicial = 200 ms)
+    int generacion = 0; //Contador de generaciones
     nodelay(ventana, TRUE); //Para que el wgetch no se bloquee esperando una tecla
     
+    //Dibujar cajita de estados y botones
+    actualizarPanel(ventana, inicio_panel, win_ancho, generacion, delay_ms, correr);
+
     //Funcionamiento de las teclas completo
     int ch;
     while (1) {
@@ -84,24 +109,31 @@ int main (int argc, char ** argv)
                 break;
             } else if (ch == ' ') { //SPACE avanzar una generación
                 calcularCuadriculaSiguiente(cuadricula);
+                generacion++;
                 dibujarCuadricula(ventana, cuadricula);
+                actualizarPanel(ventana, inicio_panel, win_ancho, generacion, delay_ms, correr);
                 wrefresh(ventana);
             } else if (ch == 'p') { //p Play o Pausa 
                 correr = !correr; //Alternanr entre 0 y 1
+                actualizarPanel(ventana, inicio_panel, win_ancho, generacion, delay_ms, correr);
             } else if (ch == '+') { //+ Acelerar (se reduce el delay)
                 if (delay_ms > 50 ){
                     delay_ms -= 50;
+                    actualizarPanel(ventana, inicio_panel, win_ancho, generacion, delay_ms, correr);
                 }
             } else if (ch == '-') { //- Desaelerar (aumenta el delay)
                 if (delay_ms < 1000 ){
                     delay_ms += 50;
+                    actualizarPanel(ventana, inicio_panel, win_ancho, generacion, delay_ms, correr);
                 }
             }
         }
         //Cuando esta en modo correr, que avance automáticamente
         if (correr) {
             calcularCuadriculaSiguiente(cuadricula);
+            generacion++;
             dibujarCuadricula(ventana, cuadricula);
+            actualizarPanel(ventana, inicio_panel, win_ancho, generacion, delay_ms, correr);
             wrefresh(ventana);
         }
         napms(delay_ms);
